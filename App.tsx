53d1477ae6754a5b9f1c4721649e29c56debd096
euviniciuss <vinicius.costa@ceuma.br>
2022-11-27 21:36:52 -0300

    feat: implementando lógica para carregamento de fonte

--- App.tsx
+++ App.tsx
@@ -1,13 +1,17 @@
 import { NativeBaseProvider } from 'native-base'
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto'
 
 import Login from './src/pages/Login'
+import { Loading } from './src/components/shared/layout'
 
 import { THEME } from './src/styles/theme'
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
+
   return (
     <NativeBaseProvider theme={THEME}>
-      <Login />
+      { fontsLoaded ? <Login /> : <Loading /> }
     </NativeBaseProvider>
   );
 }
