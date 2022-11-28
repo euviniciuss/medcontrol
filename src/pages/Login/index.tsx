6233d041831d0d77afee6d5a626ff0bdb66dae21
euviniciuss <vinicius.costa@ceuma.br>
2022-11-27 22:13:50 -0300

    feat: configurações relacionadas a svg

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,10 +1,9 @@
-import React from 'react';
-import { View, Text } from 'react-native'
+import { VStack, Heading } from 'native-base'
 
 export default function Login() {
   return(
-    <View>
-      <Text>Hello Login!</Text>
-    </View>
+    <VStack flex={1} alignItems="center" bg="gray.600" px={8} pt={24}>
+      <Heading color="gray.100" fontSize="xl" mt={20} mb={6}>Hello Login</Heading>
+    </VStack>
   )
 }
